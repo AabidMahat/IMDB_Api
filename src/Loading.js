fc71cb42a1fbdc939c096b019aa8ee9b6b58f629
Aabid Mahat <aabidmahat72@gmail.com>
2024-01-14 16:57:06 +0530

    Final Commit

--- src/Loading.js
+++ src/Loading.js
@@ -1,3 +1,26 @@
+import { BallTriangle } from "react-loader-spinner";
+
 export default function Loading() {
-  return <p className="loader">Loading...</p>;
+  return (
+    <div style={styles.container}>
+      <BallTriangle
+        height={100}
+        width={100}
+        radius={5}
+        color="#3498db"
+        ariaLabel="ball-triangle-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </div>
+  );
 }
+const styles = {
+  container: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
